904746fff447aa1b7cd170788b99bf8314e7df5e
Wil Koetsier <wbkoetsier@users.noreply.github.com>
2021-03-09 13:18:11 +0100

    Add default quickstart codewars module

---
+++ codewars/src/main/java/nl/wilkoetsier/javasnippets/App.java
@@ -0,0 +1,13 @@
+package nl.wilkoetsier.javasnippets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
